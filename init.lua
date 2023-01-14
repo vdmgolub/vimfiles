6f51993f571c42d85dd9af4e583e47d211a44e81
Vadim Golub <git@vdmgolub.com>
2023-01-14 23:05:07 +0100

    Update keymapping for diagnostics float

--- init.lua
+++ init.lua
@@ -306,7 +306,7 @@ require('nvim-treesitter.configs').setup {
 -- Diagnostic keymaps
 vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', 'L', vim.diagnostic.open_float)
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
 
 -- LSP settings.
