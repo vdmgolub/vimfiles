14ad76c018a36af2625a509488e97a544937995c
Vadim Golub <github@vdmgolub.com>
2023-10-15 20:57:24 +0200

    feat: Switch to lazy
    Based on https://github.com/josean-dev/dev-environment-files/tree/main/.config/nvim

---
+++ init.lua
@@ -0,0 +1,2 @@
+require("vdmgolub.core")
+require("vdmgolub.lazy")
---
+++ lua/vdmgolub/plugins/colorscheme.lua
@@ -0,0 +1,21 @@
+-- return {
+-- 	"bluz71/vim-nightfly-guicolors",
+-- 	priority = 1000,
+-- 	-- make sure to load this before all the other start plugin
+-- 	config = function()
+-- 		-- load the colorscheme here
+-- 		vim.cmd([[colorscheme nightfly]])
+-- 	end,
+-- }
+
+return {
+	"folke/tokyonight.nvim",
+	priority = 1000,
+	-- make sure to load this before all the other start plugin
+	config = function()
+		-- load the colorscheme here
+		vim.cmd([[colorscheme tokyonight]])
+	end,
+}
+
+-- lazy.lua references the colorscheme
