7af3d6a847db87ecc90e2b4ea5a74518dadb4332
Vadim Golub <git@vdmgolub.com>
2025-04-12 20:58:35 +0000

    feat: Change colorscheme to catppuccin

--- lua/vdmgolub/plugins/colorscheme.lua
+++ lua/vdmgolub/plugins/colorscheme.lua
@@ -9,13 +9,24 @@
 -- }
 
 return {
-  "folke/tokyonight.nvim",
-  priority = 1000,
-  -- make sure to load this before all the other start plugin
-  config = function()
-    -- load the colorscheme here
-    vim.cmd([[colorscheme tokyonight]])
-  end,
+  {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    -- make sure to load this before all the other start plugin
+    config = function()
+      -- load the colorscheme here
+      -- vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme catppuccin-mocha]])
+    end,
+  },
 }
 
 -- lazy.lua references the colorscheme
