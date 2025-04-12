1ec27e383b37e7118c6f3b72381ccb12315100d5
Vadim Golub <git@vdmgolub.com>
2025-04-12 21:05:01 +0000

    feat: Add git-blame; add keymaps to navigate git changes

--- lua/vdmgolub/plugins/git.lua
+++ lua/vdmgolub/plugins/git.lua
@@ -2,7 +2,40 @@ return {
   {
     "lewis6991/gitsigns.nvim",
     event = { "BufReadPre", "BufNewFile" },
-    config = true,
+    config = function()
+      require("gitsigns").setup({
+        on_attach = function(bufnr)
+          local gs = package.loaded.gitsigns
+
+          local function map(mode, l, r, opts)
+            opts = opts or {}
+            opts.buffer = bufnr
+            vim.keymap.set(mode, l, r, opts)
+          end
+
+          -- Navigation
+          map("n", "]c", function()
+            if vim.wo.diff then
+              return "]c"
+            end
+            vim.schedule(function()
+              gs.next_hunk()
+            end)
+            return "<Ignore>"
+          end, { expr = true })
+
+          map("n", "[c", function()
+            if vim.wo.diff then
+              return "[c"
+            end
+            vim.schedule(function()
+              gs.prev_hunk()
+            end)
+            return "<Ignore>"
+          end, { expr = true })
+        end,
+      })
+    end,
   },
   {
     "ruifm/gitlinker.nvim",
@@ -21,5 +54,16 @@ return {
     config = {
       private = true, -- All gists will be private, you won't be prompted again
     },
+  },
+  {
+    "f-person/git-blame.nvim",
+    event = "VeryLazy",
+    opts = {
+      enabled = false,
+    },
+    config = true,
+  },
+  {
+    "tpope/vim-fugitive",
   },
 }
